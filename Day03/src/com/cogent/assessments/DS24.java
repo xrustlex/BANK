1507cb60e0e392422bb810bad24e8191af5025c4
dick <dick@mbp-vadim.lan>
2022-09-21 15:19:02 -0400

    DS2.4

---
+++ Day03/src/com/cogent/assessments/DS24.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.cogent.assessments;
+
+/**
+ * @author dick
+ *
+ *         21 сент. 2022 г.
+ */
+public class DS24 {
+
+	/**
+	 * @param args
+	 */
+	static void print(int n) {
+		System.out.println(n);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int NUM = 13;
+		for (int i = 1; i <= NUM; i++)
+			print(NUM * i);
+	}
+
+}
