ba54ceea4db86d211b5b53212721b793549c34af
dick <dick@mbp-vadim.lan>
2022-09-21 15:13:30 -0400

    DS2.3

---
+++ Day03/src/com/cogent/assessments/DS23.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.cogent.assessments;
+
+/**
+ * @author dick
+ *
+ *         21 сент. 2022 г.
+ */
+public class DS23 {
+
+	/**
+	 * @param args
+	 */
+	final static int MAX = 10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i=1; i<=MAX; i++) sum += i;
+		System.out.println(sum);
+	}
+}
