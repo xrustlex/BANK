e166fcb7780fb964067179fc8945f8d0f6c5d728
dick <dick@mbp-vadim.lan>
2022-09-21 10:19:34 -0400

    3

---
+++ Day03/src/operators/FindGreaterNumber.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package operators;
+
+/**
+ * @author dick
+ *
+ * 21 сент. 2022 г.
+ */
+public class FindGreaterNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		char res = ' ';
+		if (a > b) res = 'a';
+		else res = 'b';
+		System.out.println("The greater number is " + res);
+	}
+
+}
