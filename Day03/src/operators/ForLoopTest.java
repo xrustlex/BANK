23427b1a87d46e425fbd58e7d8b334edf12b1a2f
dick <dick@mbp-vadim.lan>
2022-09-21 11:46:48 -0400

    for loop

---
+++ Day03/src/operators/ForLoopTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package operators;
+
+/**
+ * @author dick
+ *
+ *         21 сент. 2022 г.
+ */
+public class ForLoopTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (char c = 'A'; c <= 'z'; c++)
+			System.out.print(c);
+
+	}
+
+}
