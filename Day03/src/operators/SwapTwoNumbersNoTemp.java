78b5ee7fe6fb770cfea4519b3b5af720abd88527
dick <dick@mbp-vadim.lan>
2022-09-21 10:32:34 -0400

    5

---
+++ Day03/src/operators/SwapTwoNumbersNoTemp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package operators;
+
+/**
+ * @author dick
+ *
+ * 21 сент. 2022 г.
+ */
+public class SwapTwoNumbersNoTemp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println(a + "  " + b);
+	}
+
+}
