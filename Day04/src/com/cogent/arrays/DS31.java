6b778f77e2f6aacf2aff7aec3daf0b10f774634e
dick <dick@mbp-vadim.lan>
2022-09-22 16:31:12 -0400

    init

---
+++ Day04/src/com/cogent/arrays/DS31.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.cogent.arrays;
+
+/**
+ * @author dick
+ *
+ * 22 сент. 2022 г.
+ */
+class DS31 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
