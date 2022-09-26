c99e56962c0f2f683587ca3b36ca48919d23cbe1
dick <dick@mbp-vadim.lan>
2022-09-26 17:02:25 -0400

    final

--- Day07_Project/src/com/cogent/repo/CustomerRepo.java
+++ Day07_Project/src/com/cogent/repo/CustomerRepo.java
@@ -31,17 +31,17 @@ public class CustomerRepo {
 	public void viewAllCustomer() {
 
 		System.out.println("\tCustomer Id\tCustomer Name");
-		for (int i = 0; i < custArr.length; i++)
+		for (int i = 0; i < size - 11
+				; i++)
 			System.out.println("\t\t" + custArr[i].getCustomerId() + "\t\t" + custArr[i].getCustomerName());
 
 	}
 
 	public void searchCustomerById(long id) {
-		
+
 		System.out.println("\tCustomer Id\tCustomer Name");
-		for (int i = 0; i < custArr.length; i++)
-			if (id == custArr[i].getCustomerId())
-				System.out.println("\t\t" + custArr[i].getCustomerId() + "\t\t" + custArr[i].getCustomerName());
+		for (int i = 0; i < size - 1; i++)
+			System.out.println("\t\t" + custArr[i].getCustomerId() + "\t\t" + custArr[i].getCustomerName());
 
 	}
 }
