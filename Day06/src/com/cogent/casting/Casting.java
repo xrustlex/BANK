df7e51dbd0ff1797633afb4ee44336ccbf5f6186
dick <dick@mbp-vadim.lan>
2022-09-26 17:08:48 -0400

    Day06

---
+++ Day06/src/com/cogent/casting/Casting.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.cogent.casting;
+
+/**
+ * @author dick
+ *
+ * 26 сент. 2022 г.
+ */
+public class Casting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2024;
+		byte b = (byte) i;
+		System.out.println(b);
+
+	}
+
+}
---
+++ Day06/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dick
+ *
+ */
+module Day06 {
+}
