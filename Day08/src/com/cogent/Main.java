f41963d62a8dfc0dc39a3f4fc77e9923a7690ed6
dick <dick@mbp-vadim.lan>
2022-09-28 09:10:17 -0400

    init day 8

---
+++ Day08/src/com/cogent/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.cogent;
+
+/**
+ * @author dick
+ *
+ * 28 сент. 2022 г.
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
