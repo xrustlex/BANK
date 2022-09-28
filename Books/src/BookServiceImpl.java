436fe2e9e39cbae383915e12c56e09a6679de575
dick <dick@mbp-vadim.lan>
2022-09-28 12:59:30 -0400

    2

--- Books/src/BookServiceImpl.java
+++ Books/src/BookServiceImpl.java
@@ -1,19 +1,23 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import com.cogent.bean.BookBean;
 
 public class BookServiceImpl implements BookService {
 
+	List<BookBean> books = new ArrayList<>();
+
 	@Override
 	public void addBook(BookBean bookBean) {
-		// TODO Auto-generated method stub
-		
+
+		books.add(bookBean);
+
 	}
 
 	@Override
 	public void deleteBookById(int bookId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -33,6 +37,5 @@ public class BookServiceImpl implements BookService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 }
