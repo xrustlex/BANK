b171c97b3633902b3f0bb61641a9ff8f4b4d82f5
dick <dick@mbp-vadim.lan>
2022-10-03 11:47:27 -0400

    2

--- TestPlayground/src/com/cogent/test/Test.java
+++ TestPlayground/src/com/cogent/test/Test.java
@@ -16,15 +16,18 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = { 1, 3, 5, 6, 4, 2, 8, 0, 9, 7 };
+
+		int[] arr = { 1, 3, 5, 6, 4, 2, 8, 0, 9, 7, 55, -45, 9056 };
+
+		System.out.print("INITIAL ARRAY IS: ");
 		for (int e : arr)
-			System.out.print(e + " ");
+			System.out.print(" " + e + " ");
 		System.out.println();
-		System.out.println("-------------------");
+		
+		System.out.print(" SORTED ARRAY IS: ");
 		Arrays.sort(arr);
 		for (int e : arr)
-			System.out.print(e + " ");
+			System.out.print(" " + e + " ");
 	}
 
 }
