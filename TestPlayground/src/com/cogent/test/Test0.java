283019f2635473933edeb8957dcde177319320c5
dick <dick@mbp-vadim.lan>
2022-10-04 14:57:46 -0400

    13

---
+++ TestPlayground/src/com/cogent/test/Test0.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cogent.test;
+
+/**
+ * @author dick
+ *
+ *         4 окт. 2022 г.
+ */
+public class Test0 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
