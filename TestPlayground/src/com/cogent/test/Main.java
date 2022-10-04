74504ba73f496fea150a2b24f8d9e24f69e0f4c3
dick <dick@mbp-vadim.lan>
2022-10-04 12:23:58 -0400

    12

--- TestPlayground/src/com/cogent/test/Main.java
+++ TestPlayground/src/com/cogent/test/Main.java
@@ -15,13 +15,14 @@ public class Main {
 		String forS = "";
 		String revS = "";
 		
+		System.out.print("ENTER YOUR STRING\t");
 		Scanner scan = new Scanner(System.in);
 		forS = scan.nextLine();
 		
 		for (int i = forS.length() - 1; i >= 0; i--)
 			revS += forS.charAt(i);
 
-		//System.out.println(forS);
+		System.out.print("YOUR REVERSED STRING\t");
 		System.out.println(revS);
 
 	}
---
+++ TestPlayground/src/com/cogent/test/Test1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.cogent.test;
+
+/**
+ * @author dick
+ *
+ *         4 окт. 2022 г.
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "    HELLO MY CRAZY WORLD!    ";
+		String s1 = s.trim();
+		String[] arr = s1.split(" ");
+		for (int i = arr.length - 1; i >= 0; --i)
+			System.out.println(arr[i] + " ");
+
+	}
+
+}
