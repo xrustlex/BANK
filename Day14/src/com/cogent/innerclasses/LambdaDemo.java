52f20f5e93dc7eda4fd324c4be138fcfbe6820c7
dick <dick@mbp-vadim.lan>
2022-10-06 12:40:50 -0400

    2

---
+++ Day14/src/com/cogent/innerclasses/LambdaDemo.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.cogent.innerclasses;
+
+@FunctionalInterface
+interface Drawable {
+	
+	public void draw();
+
+}
+
+public class LambdaDemo {
+	
+	public static void main(String[] args) {
+		
+		int width = 10;
+
+		//Lambda
+		Drawable d2 = () -> {
+			
+			System.out.println("Drawing " + width);
+			
+		};
+		d2.draw();
+		
+	}
+}
