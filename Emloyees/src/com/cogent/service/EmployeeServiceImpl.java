1a001d722382ed172e61d2d88cfcb36752093fbc
dick <dick@mbp-vadim.lan>
2022-10-10 14:43:10 -0400

    JDBC init Day 16

--- Emloyees/src/com/cogent/service/EmployeeServiceImpl.java
+++ Emloyees/src/com/cogent/service/EmployeeServiceImpl.java
@@ -14,46 +14,40 @@ import com.cogent.repo.EmployeeRepoImpl;
 public class EmployeeServiceImpl implements EmployeeService {
 
 	EmployeeRepo employeeRepo = new EmployeeRepoImpl();
+
 	@Override
 	public void addEmployee(EmployeeBean employeeBean) {
-		// TODO Auto-generated method stub
 		employeeRepo.addEmloyee(employeeBean);
 	}
 
 	@Override
 	public List<EmployeeBean> viewAllEmloyees() {
-		// TODO Auto-generated method stub
 		return employeeRepo.viewAllEmloyees();
 	}
 
 	@Override
 	public EmployeeBean findById(long id) {
-		// TODO Auto-generated method stub
 		return employeeRepo.findById(id);
 	}
 
 	@Override
 	public void deleteById(long id) {
-		// TODO Auto-generated method stub
 		employeeRepo.deleteById(id);
 	}
 
 	@Override
 	public EmployeeBean findYoungest() {
-		// TODO Auto-generated method stub
 		return employeeRepo.findYoungest();
 	}
 
 	@Override
 	public List<EmployeeBean> findByCountry(String country) {
-		// TODO Auto-generated method stub
 		return employeeRepo.findByCountry(country);
 	}
 
 	@Override
 	public List<EmployeeBean> findByCity(String country, String city) {
-		// TODO Auto-generated method stub
 		return employeeRepo.findByCity(country, city);
 	}
-	
+
 }
---
+++ JDBC/src/com/cogent/jdbc/MainJDBC.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.cogent.jdbc;
+
+/**
+ * @author dick
+ *
+ * 10 окт. 2022 г.
+ */
+public class MainJDBC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
