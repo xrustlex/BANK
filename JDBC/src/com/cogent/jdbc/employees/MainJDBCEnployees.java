07a8b70ea4cef3f0249b641b6e59920bf6785eca
dick <dick@mbp-vadim.lan>
2022-10-11 16:30:38 -0400

    2

---
+++ JDBC/src/com/cogent/jdbc/employees/MainJDBCEnployees.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.cogent.jdbc.employees;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * @author dick
+ *
+ *         11 окт. 2022 г.
+ */
+public class MainJDBCEnployees {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int num = 1;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		Scanner scan = new Scanner(System.in);
+
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Class is available\n");
+			System.out.println();
+			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/TestDB?characterEncoding=utf8", "root", "Vadim123");
+			ps = conn.prepareStatement("INSERT INTO EMPLOYEES VALUES (?,?,?)");
+			
+			System.out.println("ENTER eID\t");
+			ps.setInt(1, scan.nextInt());
+			
+			System.out.println("ENTER eNAME\t");
+			ps.setString(2, scan.next());
+			
+			System.out.println("ENTER eDept\t");
+			ps.setString(3, scan.next());
+			
+			System.out.println("TOTAL RECORDS UPDATED:\t" + ps.executeUpdate());
+
+		} catch (SQLException | ClassNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+
+}
--- JDBC/src/com/cogent/preparestatement/MainPrepareStatement.java
+++ JDBC/src/com/cogent/preparestatement/MainPrepareStatement.java
@@ -4,12 +4,12 @@
 package com.cogent.preparestatement;
 
 import java.sql.*;
-
+import java.util.Scanner;
 
 /**
  * @author dick
  *
- * 11 окт. 2022 г.
+ *         11 окт. 2022 г.
  */
 public class MainPrepareStatement {
 
@@ -18,26 +18,41 @@ public class MainPrepareStatement {
 	 */
 	public static void main(String[] args) {
 
+		int c = 1;
+		Connection conn;
+		PreparedStatement ps;
+		Scanner scan = new Scanner(System.in);
+
 		try {
-			
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Class is available\n");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?characterEncoding=utf8", "root", "Vadim123");
-			
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO STUDENTS VALUES(?,?,?)");//SQL query as string
-			
-			ps.setInt(1, 1004);
-			ps.setString(2, "Dan");
-			ps.setString(3, "dan@test.jdbc");
-			
-			int n = ps.executeUpdate();
-			System.out.println("TOTAL RECORDS UPDATED:\t" + n);
-			
-		} catch (ClassNotFoundException | SQLException e) {
 
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?characterEncoding=utf8", "root", "Vadim123");
+			ps = conn.prepareStatement("INSERT INTO STUDENTS VALUES(?,?,?)");// SQL query as a string
+
+			while (c != 0) {
+
+				Class.forName("com.mysql.jdbc.Driver");
+				System.out.println("Class is available\n");
+
+				System.out.println("ENTER STUDENT ID:\t");
+				ps.setInt(1, scan.nextInt());
+
+				System.out.println("ENTER STUDENT NAME:\t");
+				ps.setString(2, scan.next());
+
+				System.out.println("ENTER STUDENT EMAIL:\t");
+				ps.setString(3, scan.next());
+
+				int n = ps.executeUpdate();
+				System.out.println("TOTAL RECORDS UPDATED:\t" + n);
+
+				System.out.println("ENTER 0 TO EXIT");
+				System.out.println("ENTER 1 TO ADD ANOTHER\n");
+				c = scan.nextInt();
+			}
+		} catch (Exception e) {
+			
 			e.printStackTrace();
-
+			
 		}
 	}
-
 }
