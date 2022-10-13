c974d0adad50dbc7d59326d234a0c796bb6cb518
dick <dick@localhost>
2022-10-13 01:14:29 -0400

    WRITE TO FILE done

--- JDBC/src/com/cogent/jdbc/students/MainJDBCStudents.java
+++ JDBC/src/com/cogent/jdbc/students/MainJDBCStudents.java
@@ -16,28 +16,26 @@ public class MainJDBCStudents {
 
 	public static void main(String[] args) {
 
+		InputStream configFile = null;
 		Properties prop = new Properties();
-		InputStream configFile;
-		Scanner scan = new Scanner(System.in);
-
-		List<StudentBean> studentList = new ArrayList();
+	
+		List<StudentBean> studentList = new ArrayList<StudentBean>();
 
 		try {
-
+			
 			configFile = new FileInputStream(
 					"/Users/dick/git/cogent/JDBC/src/com/cogent/jdbc/students/config.properties");
 			prop.load(configFile);
 			Class.forName(prop.getProperty("driver"));
 
-		} catch (ClassNotFoundException | IOException e) {
+		} catch (ClassNotFoundException | IOException e) { e.printStackTrace(); }
 
-			e.printStackTrace();
-
-		}
-
-		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+		try (
+				Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
 				PreparedStatement ps = conn.prepareStatement("INSERT INTO STUDENTS VALUES (?,?,?)");) {
+			
+			Scanner scan = new Scanner(System.in);
 
 			System.out.println("\n");
 			System.out.println("Class is available\n");
@@ -51,41 +49,73 @@ public class MainJDBCStudents {
 			System.out.println("ENTER STUDENT EMAIL\t");
 			ps.setString(3, scan.next());
 
+			System.out.println("\n");
 			System.out.println("TOTAL RECORDS UPDATED:\t" + ps.executeUpdate());
-
+			
 			scan.close();
 			ps.close();
 			conn.close();
-
-		} catch (SQLException e) {
-
-			e.printStackTrace();
-
-		}
+			
+		} catch (SQLException e) { e.printStackTrace(); }
+		
 		// TODO READ STUDENTS FROM DARABASE
-		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+		try (
+				Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery("SELECT * FROM STUDENTS");) {
-
+			
 			while (rs.next()) {
 
 				StudentBean s = new StudentBean(rs.getInt(1), rs.getString(2), rs.getString(3));
+				/*
+				s.setStudentID(rs.getInt(1));
+				s.setStudentName(rs.getString(2));
+				s.setStudentEmail(rs.getString(3));
+				*/
 				studentList.add(s);
 
 			}
+			// PRINTING STUDENT LIST TO CONSOLE
+			studentList.forEach(System.out::println);
 			
-		} catch (SQLException e) {
+			// SAVING TO students.dat FILE
+			File f = new File("students.bin");
+			FileOutputStream fos = new FileOutputStream(f);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(studentList);
+			
+			oos.close();
+			conn.close();
+			
+		} catch (SQLException | IOException e) {
 
 			e.printStackTrace();
 
 		}
-		// PRINTING  STUDENT LIST TO CONSOLE
-		studentList.forEach(System.out::println);
-		// TODO TO SAVE TO students.dat FILE
-		
-		// TODO PRINT FROM students.dat FILE
 
+		// TODO PRINT FROM students.dat FILE
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
