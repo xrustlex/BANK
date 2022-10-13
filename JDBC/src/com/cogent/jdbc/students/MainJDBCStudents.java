ce966983bcf820bf7412737e28ee052cb6b7d11b
dick <dick@localhost>
2022-10-13 01:31:37 -0400

    finished

--- JDBC/src/com/cogent/jdbc/students/MainJDBCStudents.java
+++ JDBC/src/com/cogent/jdbc/students/MainJDBCStudents.java
@@ -23,8 +23,7 @@ public class MainJDBCStudents {
 
 		try {
 			
-			configFile = new FileInputStream(
-					"/Users/dick/git/cogent/JDBC/src/com/cogent/jdbc/students/config.properties");
+			configFile = new FileInputStream("config.properties");
 			prop.load(configFile);
 			Class.forName(prop.getProperty("driver"));
 
@@ -58,7 +57,7 @@ public class MainJDBCStudents {
 			
 		} catch (SQLException e) { e.printStackTrace(); }
 		
-		// TODO READ STUDENTS FROM DARABASE
+		// READING STUDENTS FROM DARABASE
 		try (
 				Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
@@ -67,13 +66,7 @@ public class MainJDBCStudents {
 			
 			while (rs.next()) {
 
-				StudentBean s = new StudentBean(rs.getInt(1), rs.getString(2), rs.getString(3));
-				/*
-				s.setStudentID(rs.getInt(1));
-				s.setStudentName(rs.getString(2));
-				s.setStudentEmail(rs.getString(3));
-				*/
-				studentList.add(s);
+				studentList.add(new StudentBean(rs.getInt(1), rs.getString(2), rs.getString(3)));
 
 			}
 			// PRINTING STUDENT LIST TO CONSOLE
@@ -85,16 +78,23 @@ public class MainJDBCStudents {
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(studentList);
 			
+			// TODO PRINT FROM students.dat FILE
+			FileInputStream fis = new FileInputStream(f);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Object obj = ois.readObject();
+			List<StudentBean> sb = (ArrayList<StudentBean>) obj;
+			
+			sb.forEach(System.out::println);
+			
+			ois.close();
 			oos.close();
 			conn.close();
 			
-		} catch (SQLException | IOException e) {
+		} catch (SQLException | IOException | ClassNotFoundException e) {
 
 			e.printStackTrace();
 
 		}
-
-		// TODO PRINT FROM students.dat FILE
 		
 	}
 
