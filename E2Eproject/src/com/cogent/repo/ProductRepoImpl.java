8d007c55405fa2c3dccc978d8189bd67ec227eff
dick <dick@mbp-vadim.lan>
2022-10-15 13:21:20 -0400

    final

--- E2Eproject/src/com/cogent/repo/ProductRepoImpl.java
+++ E2Eproject/src/com/cogent/repo/ProductRepoImpl.java
@@ -59,7 +59,7 @@ public class ProductRepoImpl implements ProductRepo {
 
 		InputStream configFile = null;
 		Properties prop = new Properties(null);
-		// DELETE FROM products WHERE id = ?
+		// LOADING FROM config.properties FILE
 		try {
 
 			configFile = new FileInputStream("config.properties");
@@ -69,6 +69,7 @@ public class ProductRepoImpl implements ProductRepo {
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		// DELETE FROM products WHERE id = ?
 		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
 				PreparedStatement ps = conn.prepareStatement("DELETE FROM products WHERE id = ?");) {
@@ -89,7 +90,7 @@ public class ProductRepoImpl implements ProductRepo {
 
 		InputStream configFile = null;
 		Properties prop = new Properties(null);
-		// DELETE FROM PRODUCTS WHERE categ = ?
+		// LOADING FROM config.properties FILE
 		try {
 
 			configFile = new FileInputStream("config.properties");
@@ -99,6 +100,7 @@ public class ProductRepoImpl implements ProductRepo {
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		// DELETE FROM PRODUCTS WHERE categ = ?
 		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
 				PreparedStatement ps = conn.prepareStatement("DELETE FROM products WHERE categ = ?");) {
@@ -120,7 +122,7 @@ public class ProductRepoImpl implements ProductRepo {
 		ProductBean product = null;
 		InputStream configFile = null;
 		Properties prop = new Properties(null);
-		// SELECT * FROM products WHERE id = ?
+		// LOADING FROM config.properties FILE
 		try {
 
 			configFile = new FileInputStream("config.properties");
@@ -130,6 +132,7 @@ public class ProductRepoImpl implements ProductRepo {
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		// SELECT * FROM products WHERE id = ?
 		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
 				PreparedStatement ps = conn.prepareStatement("SELECT * FROM products WHERE id = ?");) {
@@ -140,6 +143,9 @@ public class ProductRepoImpl implements ProductRepo {
 			product = new ProductBean(rs.getLong("ID"), rs.getString("name"), rs.getString("categ"),
 					rs.getBigDecimal("price"), rs.getDate("made"), rs.getDate("exp"));
 
+			ps.close();
+			conn.close();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -149,22 +155,36 @@ public class ProductRepoImpl implements ProductRepo {
 	@Override
 	public List<ProductBean> findCheapestInCat(String cat) {
 
-		ProductBean product = null;
 		InputStream configFile = null;
 		Properties prop = new Properties(null);
 		List<ProductBean> products = new ArrayList<>();
+		// LOADING FROM config.properties FILE
+		try {
 
-		// SELECT * FROM products WHERE price = (SELECT MIN(price) FROM products)
+			configFile = new FileInputStream("config.properties");
+			prop.load(configFile);
+			Class.forName(prop.getProperty("driver"));
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// SELECT * FROM products WHERE price = (SELECT MIN(price) FROM products WHERE
+		// categ = ?)
 		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
 				prop.getProperty("password"));
-				PreparedStatement ps = conn
-						.prepareStatement("SELECT * FROM products WHERE price = (SELECT MIN(price) FROM products)");) {
+				PreparedStatement ps = conn.prepareStatement(
+						"SELECT * FROM products WHERE price = (SELECT MIN(price) FROM products WHERE categ = ?");) {
 
+			ps.setString(1, cat);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next())
 
 				products.add(new ProductBean(rs.getLong("ID"), rs.getString("name"), rs.getString("categ"),
 						rs.getBigDecimal("price"), rs.getDate("made"), rs.getDate("exp")));
+
+			rs.close();
+			ps.close();
+			conn.close();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -174,20 +194,115 @@ public class ProductRepoImpl implements ProductRepo {
 
 	@Override
 	public List<ProductBean> findProductsByCat(String cat) {
-		// TODO Auto-generated method stub
-		return null;
+
+		InputStream configFile = null;
+		Properties prop = new Properties(null);
+		List<ProductBean> products = new ArrayList<>();
+		// LOADING FROM config.properties FILE
+		try {
+
+			configFile = new FileInputStream("config.properties");
+			prop.load(configFile);
+			Class.forName(prop.getProperty("driver"));
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// SELECT * FROM products WHERE categ = '?'
+		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+				prop.getProperty("password"));
+				PreparedStatement ps = conn.prepareStatement("SELECT * FROM products WHERE categ = '?'")) {
+
+			ps.setString(1, cat);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				// ADDING TO LIST
+				products.add(new ProductBean(rs.getLong("ID"), rs.getString("name"), rs.getString("categ"), rs.getBigDecimal("price"), rs.getDate("made"), rs.getDate("exp")));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return products;
 	}
 
 	@Override
 	public void updateProduct(ProductBean product) {
-		// TODO Auto-generated method stub
+
+		InputStream configFile = null;
+		Properties prop = new Properties(null);
+		// LOADING FROM config.properties FILE
+		try {
+
+			configFile = new FileInputStream("config.properties");
+			prop.load(configFile);
+			Class.forName(prop.getProperty("driver"));
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// UPDATE products SET name = ?, categ = ?, price = ?, made = ?, exp = ? WHERE
+		// ID = ?
+		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+				prop.getProperty("password"));
+				PreparedStatement ps = conn.prepareStatement(
+						"UPDATE products SET name = ?, categ = ?, price = ?, made = ?, exp = ? WHERE ID = ?")) {
+
+			ps.setString(1, product.getName());
+			ps.setString(2, product.getCateg());
+			ps.setBigDecimal(3, product.getPrice());
+
+			ps.setDate(4, product.getMade());
+			ps.setDate(5, product.getExp());
+			ps.setLong(6, product.getId());
+
+			ps.executeUpdate();
+			ps.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public List<ProductBean> findExpiredProducts() {
-		// TODO Auto-generated method stub
-		return null;
+
+		InputStream configFile = null;
+		Properties prop = new Properties(null);
+		List<ProductBean> products = new ArrayList<>();
+		// LOADING FROM config.properties FILE
+		try {
+
+			configFile = new FileInputStream("config.properties");
+			prop.load(configFile);
+			Class.forName(prop.getProperty("driver"));
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// SELECT * FROM products WHERE exp < Current_Date()
+		try (Connection conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+				prop.getProperty("password"));
+				PreparedStatement ps = conn.prepareStatement("SELECT * FROM products WHERE exp < Current_Date()")) {
+
+			ResultSet rs = ps.executeQuery();
+			// ADDING TO LIST
+			while (rs.next()) {
+				// ADDING TO LIST
+				products.add(new ProductBean(rs.getLong("ID"), rs.getString("name"), rs.getString("categ"), rs.getBigDecimal("price"), rs.getDate("made"), rs.getDate("exp")));
+			}
+			
+			ps.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return products;
 	}
 
 }
