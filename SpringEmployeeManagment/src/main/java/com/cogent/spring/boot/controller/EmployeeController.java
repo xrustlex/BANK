1df593a664bf98c251a6e2df90c8190b07a0d8c1
dick <dick@mbp-vadim.lan>
2022-10-19 14:52:27 -0400

    2

--- SpringEmployeeManagment/src/main/java/com/cogent/spring/boot/controller/EmployeeController.java
+++ SpringEmployeeManagment/src/main/java/com/cogent/spring/boot/controller/EmployeeController.java
@@ -14,17 +14,24 @@ public class EmployeeController {
 
 	// WRITE LOGIC TO PERFORM CRUD OPERATIONS USING HTTP METHODS
 	// USE JPA REPOSITORY TO VARIOUS OPERATIONS
+	
+	// GET
+	// POST
+	// PUT
+	// DELETE
 
 	// READ
 	@Autowired
 	EmployeeRepo employeeRepo;
+	
 	// creating a get mapping that retrieves all the books detail from the database
-	@GetMapping("/employee")
+	@GetMapping("/getallemployees")
 	private List<EmployeeBean> getAllEmployees() {
 		return employeeRepo.findAll();
 	}
+	
 	// CREATE
-
+	
 	// UPDATE
 
 	// DELETE
