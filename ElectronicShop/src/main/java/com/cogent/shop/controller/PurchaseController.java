70c7fc906a5dfde636ae8053bd70df6b794ca72f
dick <dick@mbp-vadim.lan>
2022-10-21 16:35:49 -0400

    2

--- ElectronicShop/src/main/java/com/cogent/shop/controller/PurchaseController.java
+++ ElectronicShop/src/main/java/com/cogent/shop/controller/PurchaseController.java
@@ -1,5 +1,24 @@
 package com.cogent.shop.controller;
 
-public class PurchaseController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cogent.shop.entity.Purchase;
+import com.cogent.shop.repo.PurchaseRepo;
+
+@RestController
+@RequestMapping("api/purchase")
+public class PurchaseController {
+	
+	@Autowired
+	PurchaseRepo purchaseRepo;
+	
+	@PostMapping("/addpurchase")
+	Purchase addPurchase(@RequestBody Purchase purchase) {
+		return purchaseRepo.save(purchase);
+	}
+	
 }
