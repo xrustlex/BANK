dcd136e012ede7f2a44140452db86b103f8bb0c7
dick <dick@mbp-vadim.lan>
2022-10-21 16:44:35 -0400

    3

--- ElectronicShop/src/main/java/com/cogent/shop/controller/PurchaseController.java
+++ ElectronicShop/src/main/java/com/cogent/shop/controller/PurchaseController.java
@@ -1,6 +1,11 @@
 package com.cogent.shop.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +17,29 @@ import com.cogent.shop.repo.PurchaseRepo;
 @RestController
 @RequestMapping("api/purchase")
 public class PurchaseController {
-	
+
 	@Autowired
 	PurchaseRepo purchaseRepo;
-	
+
 	@PostMapping("/addpurchase")
 	Purchase addPurchase(@RequestBody Purchase purchase) {
 		return purchaseRepo.save(purchase);
 	}
-	
+
+	@GetMapping("/getpurchase")
+	List<Purchase> getAllPurchases() {
+		return purchaseRepo.findAll();
+	}
+
+	@DeleteMapping("/deletepurchase/{id}")
+	String deletePurchase(@PathVariable("id") long id) {
+		try {
+			purchaseRepo.deleteById(id);
+			return "PURCHASE DELETION SUCCESSFUL";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "PURCHASE DELETION UNSUCCESSFUL";
+		}
+	}
+
 }
