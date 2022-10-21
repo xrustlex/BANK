9f2799c58aacb71375feb6895c4b6f1cb5170181
dick <dick@mbp-vadim.lan>
2022-10-21 10:15:16 -0400

    init

--- EmpManage/src/main/java/com/example/EmpManage/controller/EmployeeController.java
+++ EmpManage/src/main/java/com/example/EmpManage/controller/EmployeeController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.EmpManage.entity.Employee;
 import com.example.EmpManage.repo.EmployRepository;
 
 @RestController
+
+@RequestMapping("api/employee")
 public class EmployeeController {
 
 	@Autowired
