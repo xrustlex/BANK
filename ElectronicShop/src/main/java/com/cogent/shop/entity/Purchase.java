cd256d3b1bea11c4ac8befdc588156d28e50cc46
dick <dick@mbp-vadim.lan>
2022-10-23 10:01:27 -0400

    2

--- ElectronicShop/src/main/java/com/cogent/shop/entity/Purchase.java
+++ ElectronicShop/src/main/java/com/cogent/shop/entity/Purchase.java
@@ -9,6 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table
 //Entity class(Purchase) can have id, producNname, cost ,dateOfPurchase, deliveryDate, customerFeedback 
