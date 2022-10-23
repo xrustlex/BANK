fee00e85df1beba065d47a0de100a6b246bceec4
dick <dick@mbp-vadim.lan>
2022-10-23 10:12:08 -0400

    3

--- ElectronicShop/src/main/java/com/cogent/shop/entity/Purchase.java
+++ ElectronicShop/src/main/java/com/cogent/shop/entity/Purchase.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table
+@Table(name = "Purchases")
 //Entity class(Purchase) can have id, producNname, cost ,dateOfPurchase, deliveryDate, customerFeedback 
 public class Purchase {
 	
@@ -29,50 +29,5 @@ public class Purchase {
 	private Date dateOfPurchase;
 	private Date deliveryDate;
 	private String customerFeedback;
-	
-	public Purchase() {
-		super();
-	}
-
-	public Purchase(long id, String productName, BigDecimal cost, Date dateOfPurchase, Date deliveryDate,
-			String customerFeedback) {
-		super();
-		this.id = id;
-		this.productName = productName;
-		this.cost = cost;
-		this.dateOfPurchase = dateOfPurchase;
-		this.deliveryDate = deliveryDate;
-		this.customerFeedback = customerFeedback;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public BigDecimal getCost() {
-		return cost;
-	}
-
-	public Date getDateOfPurchase() {
-		return dateOfPurchase;
-	}
-
-	public Date getDeliveryDate() {
-		return deliveryDate;
-	}
-
-	public String getCustomerFeedback() {
-		return customerFeedback;
-	}
-
-	@Override
-	public String toString() {
-		return "Purchase [id=" + id + ", productName=" + productName + ", cost=" + cost + ", dateOfPurchase="
-				+ dateOfPurchase + ", deliveryDate=" + deliveryDate + ", customerFeedback=" + customerFeedback + "]";
-	}
 
 }
