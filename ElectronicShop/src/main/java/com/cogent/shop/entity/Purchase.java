4d3a377d3b3471e683f9fc6f5fcfaa7b9322c87e
dick <dick@mbp-vadim.lan>
2022-10-23 10:16:39 -0400

    4

--- ElectronicShop/src/main/java/com/cogent/shop/entity/Purchase.java
+++ ElectronicShop/src/main/java/com/cogent/shop/entity/Purchase.java
@@ -9,13 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "Purchases")
 //Entity class(Purchase) can have id, producNname, cost ,dateOfPurchase, deliveryDate, customerFeedback 
@@ -29,5 +22,68 @@ public class Purchase {
 	private Date dateOfPurchase;
 	private Date deliveryDate;
 	private String customerFeedback;
+	
+	public Purchase() {
+		super();
+	}
 
+	public Purchase(long id, String productName, BigDecimal cost, Date dateOfPurchase, Date deliveryDate,
+			String customerFeedback) {
+		super();
+		this.id = id;
+		this.productName = productName;
+		this.cost = cost;
+		this.dateOfPurchase = dateOfPurchase;
+		this.deliveryDate = deliveryDate;
+		this.customerFeedback = customerFeedback;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public BigDecimal getCost() {
+		return cost;
+	}
+
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
+
+	public Date getDateOfPurchase() {
+		return dateOfPurchase;
+	}
+
+	public void setDateOfPurchase(Date dateOfPurchase) {
+		this.dateOfPurchase = dateOfPurchase;
+	}
+
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getCustomerFeedback() {
+		return customerFeedback;
+	}
+
+	public void setCustomerFeedback(String customerFeedback) {
+		this.customerFeedback = customerFeedback;
+	}
+	
 }
