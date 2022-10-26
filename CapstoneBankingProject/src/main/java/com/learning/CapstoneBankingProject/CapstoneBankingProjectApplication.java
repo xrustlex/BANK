85836d1c763a49824481fbb675268f21d51433c8
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2022-10-26 19:08:14 -0400

    Added Maven Project

---
+++ CapstoneBankingProject/src/main/java/com/learning/CapstoneBankingProject/CapstoneBankingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learning.CapstoneBankingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBankingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBankingProjectApplication.class, args);
+	}
+
+}
