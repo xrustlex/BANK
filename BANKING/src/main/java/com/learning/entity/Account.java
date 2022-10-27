75a6d12a082925065b680792f18fe056e9ffa484
dick <dick@mbp-vadim.lan>
2022-10-27 11:44:49 -0400

    class Account done

--- BANKING/src/main/java/com/learning/entity/Account.java
+++ BANKING/src/main/java/com/learning/entity/Account.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Table(name="ACCOUNTS")
 public class Account {
 /*
- * accountType: Enum(SB/CA),
+ * accountType: enum(SB/CA),
  * accountBalance: Number,
  * approved: no,
  * accountNumber: Number,
@@ -24,5 +24,69 @@ public class Account {
 	private long accountNumber;
 	private Date dateOfCreation;
 	private long customerId;
+	
+	public Account() {
+		super();
+	}
+
+	public Account(AccountType accountType, BigDecimal accountBalance, boolean approved, long accountNumber,
+			Date dateOfCreation, long customerId) {
+		super();
+		this.accountType = accountType;
+		this.accountBalance = accountBalance;
+		this.approved = approved;
+		this.accountNumber = accountNumber;
+		this.dateOfCreation = dateOfCreation;
+		this.customerId = customerId;
+	}
+
+	public AccountType getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(AccountType accountType) {
+		this.accountType = accountType;
+	}
+
+	public BigDecimal getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(BigDecimal accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	public boolean isApproved() {
+		return approved;
+	}
+
+	public void setApproved(boolean approved) {
+		this.approved = approved;
+	}
+
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Date getDateOfCreation() {
+		return dateOfCreation;
+	}
+
+	public void setDateOfCreation(Date dateOfCreation) {
+		this.dateOfCreation = dateOfCreation;
+	}
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	
 	
 }
