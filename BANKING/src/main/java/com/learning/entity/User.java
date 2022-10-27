6832fc80cc6ce9783845a5e60a7691e90cc8a058
dick <dick@mbp-vadim.lan>
2022-10-27 11:31:39 -0400

    User class done

--- BANKING/src/main/java/com/learning/entity/User.java
+++ BANKING/src/main/java/com/learning/entity/User.java
@@ -15,7 +15,56 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
+	@Column(name="username")
 	private String username;
+	@Column(name="full_name")
 	private String fullName;
+	@Column(name="password")
+	private String password;
+	
+	public User() {
+		super();
+	}
+
+	public User(long id, String username, String fullName, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.fullName = fullName;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	
 }
