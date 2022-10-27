2533620425180616d5bcbce6764614d72a65284f
dick <dick@mbp-vadim.lan>
2022-10-26 22:38:05 -0400

    init

---
+++ BANKING/src/main/java/com/cogent/eShop/AssesmantProject10251Application.java
@@ -0,0 +1,13 @@
+package com.cogent.eShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssesmantProject10251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssesmantProject10251Application.class, args);
+	}
+
+}
---
+++ BANKING/src/test/java/com/cogent/eShop/AssesmantProject10251ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogent.eShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmantProject10251ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
